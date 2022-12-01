9bb6b423150cf1027f5fb8af289bd98ef9c8eb46
“Sara <sara.raza@xprolabs.com>
2022-12-01 16:28:19 +0500

    Graphs: Legend heading is updated

--- app/javascript/components/pages/dashboard/components/services-percentage/component.jsx
+++ app/javascript/components/pages/dashboard/components/services-percentage/component.jsx
@@ -30,31 +30,31 @@ const Component = () => {
               <div className={css.perc}>
                 {stats.physically_or_mentally_abused.percentage}% ({stats.physically_or_mentally_abused.cases})
               </div>
-              <div className={css.text}>Physical Violence or Injury</div>
+              <div className={css.text}>Physical Abuse</div>
             </div>
             <div className={css.card} style={{ backgroundColor: "rgb(54, 162, 235)" }} flat>
               <div className={css.perc}>
                 {stats.street_child.percentage}% ({stats.street_child.cases})
               </div>
-              <div className={css.text}>Mental Violence</div>
+              <div className={css.text}>Psychological Violence</div>
             </div>
             <div className={css.card} style={{ backgroundColor: "rgb(128, 0, 128)" }} flat>
               <div className={css.perc}>
                 {stats.neglect_or_negligent_treatment.percentage}% ({stats.neglect_or_negligent_treatment.cases})
               </div>
-              <div className={css.text}>Neglect and Negligent Treatment</div>
+              <div className={css.text}>Neglect / Negligent Treatment</div>
             </div>
             <div className={css.card} style={{ backgroundColor: "rgb(255, 205, 86)" }} flat>
               <div className={css.perc}>
                 {stats.exploitation.percentage}% ({stats.exploitation.cases})
               </div>
-              <div className={css.text}>Exploitation</div>
+              <div className={css.text}>Economic Exploitation</div>
             </div>
             <div className={css.card} style={{ backgroundColor: "rgb(54, 170, 89)" }} flat>
               <div className={css.perc}>
                 {stats.sexual_abuse_or_exploitation.percentage}% ({stats.sexual_abuse_or_exploitation.cases})
               </div>
-              <div className={css.text}>Sexual Abuse and Sexual Exploitation</div>
+              <div className={css.text}>Sexual Abuse / Violence / Exploitation</div>
             </div>
           </div>
         </>
